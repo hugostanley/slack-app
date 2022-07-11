a35e376e007b3f113c23fe23baf7b977c7910b6b
hugostanley <stanleyhugo06@gmail.com>
2022-07-11 15:15:23 +0800

    Install axios library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import Test from './components/Test'
+import axios from "axios";
 
 function App() {
-	const [state, setState] = useState(null)
+	const [name, setName] = useState('')
+	const [pass, setPass] = useState('')
+	const [confirm, setConfirm] = useState('')
+	const [good, setGood] = useState(false)
+	const [data, setData] = useState(null)
+	const handleSubmit = (e)=> {
+		e.preventDefault()
+		if(pass === confirm){
+			setGood(true)
+			setData({
+				"email": name,
+				"password": pass,
+				"password_confirmation": confirm
+			})
+		}
+	}
+	const postIt = (data)=> {
+		axios.post('http://206.189.91.54/api/v1/auth/', data).then(res=> console.log(res)).catch(err=> console.log(err))
+	}
+	useEffect(()=>{
+		if(data){
+			postIt(data)
+		}
+	}, [data])
   return (
     <div className="App">
+			<form onSubmit={handleSubmit}>
+				<input value={name} onChange={e=> setName(e.target.value)} placeholder='name'/>
+				<input value={pass} onChange={e=> setPass(e.target.value)} placeholder='pass'/>
+				<input value={confirm} onChange={e=> setConfirm(e.target.value)} placeholder='confirme'/>
+				<button type="submit">submit</button>
+			</form>
     </div>
   );
 }
---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+
+const Test = ()=> {
+	return <div>Hello</div>
+}
+
+export default Test
