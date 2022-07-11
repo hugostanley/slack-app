36274d1aee1f64ab5cc8d1a1184b6e2c56d26d02
hugostanley <stanleyhugo06@gmail.com>
2022-07-11 20:10:11 +0800

    Setup root files for initial static code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React, {useEffect, useState} from "react";
-import Test from './components/Test'
-import axios from "axios";
+import React from "react";
+import {Routes,  Route} from 'react-router-dom'
+import LandingPage from './pages/Landingpage'
+import Chat from './pages/Chat'
 
-function App() {
-	const [name, setName] = useState('')
-	const [pass, setPass] = useState('')
-	const [confirm, setConfirm] = useState('')
-	const [good, setGood] = useState(false)
-	const [data, setData] = useState(null)
-	const handleSubmit = (e)=> {
-		e.preventDefault()
-		if(pass === confirm){
-			setGood(true)
-			setData({
-				"email": name,
-				"password": pass,
-				"password_confirmation": confirm
-			})
-		}
-	}
-	const postIt = (data)=> {
-		axios.post('http://206.189.91.54/api/v1/auth/', data).then(res=> console.log(res)).catch(err=> console.log(err))
-	}
-	useEffect(()=>{
-		if(data){
-			postIt(data)
-		}
-	}, [data])
+const App=()=> {
   return (
-    <div className="App">
-			<form onSubmit={handleSubmit}>
-				<input value={name} onChange={e=> setName(e.target.value)} placeholder='name'/>
-				<input value={pass} onChange={e=> setPass(e.target.value)} placeholder='pass'/>
-				<input value={confirm} onChange={e=> setConfirm(e.target.value)} placeholder='confirme'/>
-				<button type="submit">submit</button>
-			</form>
-    </div>
+		<Routes>
+			<Route path="/" element={<LandingPage/>}/>
+			<Route path="/chat" element={<Chat/>}/>
+		</Routes>
   );
 }
 
