0be22eb3f126c73211ff85239a07a43189f535b2
hugostanley <stanleyhugo06@gmail.com>
2022-07-12 14:32:11 +0800

    Create initial styling for landing page navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
-import { Box, Center, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Image, Spacer, Text } from "@chakra-ui/react";
 import React from "react";
+import SlackLogo from '../assets/images/Slack_RGB.png'
 
 const NavBar = ()=> {
 	return (
-		<Flex>
-			<Center>Image</Center>
-			<Center>
+		<Flex alignItems='center'  height='100%' width='100%' >
+			<Center flex='1'>
+				<Image src={SlackLogo} boxSize='50%'/>
+			</Center>
+			<Spacer flex='2'/>
+			<Center flex='1' justifyContent='space-evenly'>
 				<Text>Text</Text>
 				<Text>Text</Text>
 				<Text>Text</Text>
 			</Center>
-			
 		</Flex>
 	)
 }
