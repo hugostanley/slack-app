31dbf8d79f32a92d15b82e516e7eb720238b43b8
hugostanley <stanleyhugo06@gmail.com>
2022-07-12 16:07:17 +0800

    Create footer layout

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {Link, Flex, Stack, Text, Spacer} from '@chakra-ui/react'
 
 const Footer =()=> {
-return <div>Footer</div>
+	return (
+		<Flex borderTop='lightgray 1px solid' gap='2' fontSize="x-small" h='100%' w='100%' paddingX='4rem' alignItems='center'>
+			<Text fontWeight='bold' >Slack 2022</Text>
+			<Text> The Slack logo is a trademark of its respective owner.</Text>
+			<Spacer flex='2' />
+			<Stack direction='row'>
+				<Link>Data Policy</Link>
+				<Link>Data Policy</Link>
+				<Link>Data Policy</Link>
+				</Stack>
+		</Flex>
+
+		) 
 }
 
 export default Footer
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,6 +2,8 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import React, { useState } from "react";
 import Header from '../layouts/Header'
 import Main from '../layouts/Main'
+import Footer from '../layouts/Footer'
+
 const LandingPage = ()=>{
 	return ( 
 		<Grid templateAreas={`"header" "main" "footer"`}
@@ -11,7 +13,7 @@ const LandingPage = ()=>{
 		>
 			<GridItem  w='100vw' area={'header'}><Header /></GridItem>
 			<GridItem w='100vw' area={'main'}><Main/></GridItem>
-			<GridItem w='100vw' area={'footer'}>Footer</GridItem>
+			<GridItem w='100vw' area={'footer'}><Footer/></GridItem>
 		</Grid>
 	)
 }
