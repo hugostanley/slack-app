5460a5145a518e095cdd03af86859f3f1c871003
hugostanley <stanleyhugo06@gmail.com>
2022-07-12 16:15:37 +0800

    Insert external links to footer links

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,14 +4,14 @@ import {Link, Flex, Stack, Text, Spacer} from '@chakra-ui/react'
 const Footer =()=> {
 	return (
 		<Flex borderTop='lightgray 1px solid' gap='2' fontSize="x-small" h='100%' w='100%' paddingX='4rem' alignItems='center'>
-			<Text fontWeight='bold' >Slack 2022</Text>
+			<Text fontWeight='bold' >©2022 Slack Technologies</Text>
 			<Text> The Slack logo is a trademark of its respective owner.</Text>
 			<Spacer flex='2' />
 			<Stack direction='row'>
-				<Link>Data Policy</Link>
-				<Link>Data Policy</Link>
-				<Link>Data Policy</Link>
-				</Stack>
+				<Link href="https://slack.com/trust/privacy/privacy-policy" isExternal>Data Policy</Link>
+				<Link href='https://slack.com/legal' isExternal>Terms </Link>
+				<Link  >Cookie Preferences</Link>
+			</Stack>
 		</Flex>
 
 		) 
