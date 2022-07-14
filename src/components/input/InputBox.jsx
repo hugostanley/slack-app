8bb3c311849246451e5751667c12ede5dfc0ccb9
hugostanley <stanleyhugo06@gmail.com>
2022-07-14 19:10:40 +0800

    Create an input box component

---
+++ src/components/input/InputBox.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+	Box,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputRightElement,
+} from "@chakra-ui/react";
+import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
+const InputBox = ({ id, title, type, placeholder, showPass, setShowPass }) => {
+	const handleClick = () => setShowPass(!showPass);
+	return (
+		<>
+			{type !== "password" ? (
+				<Box>
+					<FormLabel htmlFor={id}>{title}</FormLabel>
+					<Input id={id} type={type} placeholder={placeholder} />
+				</Box>
+			) : (
+				<Box>
+					<FormLabel htmlFor="password">{title}</FormLabel>
+					<InputGroup>
+						<Input
+							id="password"
+							type={showPass ? "text" : "password"}
+							placeholder={placeholder}
+						/>
+						<InputRightElement>
+							{showPass ? (
+								<ViewOffIcon onClick={handleClick} />
+							) : (
+								<ViewIcon onClick={handleClick} />
+							)}
+						</InputRightElement>
+					</InputGroup>
+				</Box>
+			)}
+		</>
+	);
+};
+
+export default InputBox;
