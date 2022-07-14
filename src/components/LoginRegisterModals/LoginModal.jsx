7f4f3916f69a4537c51892d8fb18ff3d1f8bcce3
hugostanley <stanleyhugo06@gmail.com>
2022-07-14 15:06:07 +0800

    Enable login authentication functionality

--- src/components/LoginRegisterModals/LoginModal.jsx
+++ src/components/LoginRegisterModals/LoginModal.jsx
@@ -18,16 +18,42 @@ import {
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import ProgressModal from "./ProgressModal";
+import useFetch from "../../utils/useFetch";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginModal = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [showPass, setShowPass] = useState(false);
 	const handleClick = () => setShowPass(!showPass);
 	const [loading, setLoading] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		axios
+			.post("http://206.189.91.54/api/v1/auth/sign_in", {
+				email,
+				password,
+			})
+			.then((resp) => {
+				console.log(resp);
+				navigate("/client");
+			})
+			.catch((err) => console.log(err))
+			.finally(() => setLoading(false));
+	};
+
 	return (
 		<>
 			<Button onClick={onOpen}>Log In</Button>
-			<Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={loading ? false: true}>
+			<Modal
+				isOpen={isOpen}
+				onClose={onClose}
+				closeOnOverlayClick={loading ? false : true}
+			>
 				<ModalOverlay />
 				<ModalContent>
 					{loading ? (
@@ -36,7 +62,11 @@ const LoginModal = () => {
 						<>
 							<ModalHeader>Log In</ModalHeader>
 							<ModalCloseButton />
-							<FormControl paddingX={"1.5rem"} pb="1rem">
+							<FormControl
+								onSubmit={handleSubmit}
+								paddingX={"1.5rem"}
+								pb="1rem"
+							>
 								<VStack
 									divider={<StackDivider borderColor={"gray.200"} />}
 									spacing={4}
@@ -44,12 +74,20 @@ const LoginModal = () => {
 								>
 									<Box>
 										<FormLabel htmlFor="email">Email address</FormLabel>
-										<Input id="email" type={"email"} placeholder="Your email" />
+										<Input
+											value={email}
+											onChange={(e) => setEmail(e.target.value)}
+											id="email"
+											type={"email"}
+											placeholder="Your email"
+										/>
 									</Box>
 									<Box>
 										<FormLabel htmlFor="password">Password</FormLabel>
 										<InputGroup>
 											<Input
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
 												id="password"
 												type={showPass ? "text" : "password"}
 												placeholder="Your password"
@@ -63,7 +101,13 @@ const LoginModal = () => {
 											</InputRightElement>
 										</InputGroup>
 									</Box>
-									<Button colorScheme={"yellow"}>Login</Button>
+									<Button
+										colorScheme={"yellow"}
+										type="submit"
+										onClick={handleSubmit}
+									>
+										Login
+									</Button>
 								</VStack>
 							</FormControl>
 						</>
