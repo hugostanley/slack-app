8d54e2c58af792925b2649ffdb87410152bd36a8
hugostanley <stanleyhugo06@gmail.com>
2022-07-15 17:45:21 +0800

    Set local storage keys for global variables

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { createContext, useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/index";
 import Client from "./pages/Client";
 import PrivateRoute from "routes/PrivateRoute";
+import {UserContext} from 'utils/Context'
 
 const App = () => {
+	const [userData, setUserData] = useState(false);
+	const [auth, setAuth] = useState(JSON.parse(localStorage.getItem('auth')));
+	const [headers, setHeaders] = useState(JSON.parse(localStorage.getItem('headers')));
+
+	useEffect(()=>{
+		localStorage.setItem('auth', JSON.stringify(auth))
+		localStorage.setItem('headers', JSON.stringify(headers))
+	},[auth, userData, headers])
+	
 	return (
-		<Routes>
-			<Route index path="/" element={<LandingPage />} />
-			<Route element={<PrivateRoute />}>
-				<Route path="/client" element={<Client />} />
-			</Route>
-		</Routes>
+		<UserContext.Provider value={{userData, setUserData, auth, setAuth, headers, setHeaders}}>
+			<Routes>
+				<Route index path="/" element={<LandingPage />} />
+				<Route element={<PrivateRoute log={auth} />}>
+					<Route path="/client" element={<Client />} />
+				</Route>
+				<Route path="*" element={<div>none</div>} />
+			</Routes>
+		</UserContext.Provider>
 	);
 };
 
