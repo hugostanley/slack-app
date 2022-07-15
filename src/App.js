0d2f06bc0748b44c221fba880dce51141c816f48
hugostanley <stanleyhugo06@gmail.com>
2022-07-15 13:40:01 +0800

    Render PrivateRoute component in the root app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/index";
 import Client from "./pages/Client";
+import PrivateRoute from "routes/PrivateRoute";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<LandingPage />} />
-			<Route path="/client" element={<Client />} />
+			<Route index path="/" element={<LandingPage />} />
+			<Route element={<PrivateRoute />}>
+				<Route path="/client" element={<Client />} />
+			</Route>
 		</Routes>
 	);
 };
