56413fe4595636cb100536af8a39c98a8dc9f0f4
hugostanley <stanleyhugo06@gmail.com>
2022-07-15 17:45:49 +0800

    Make the auth to true once logged in

--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import {UserContext} from 'utils/Context'
 const BASE_URL = "http://206.189.91.54/api/v1";
+
 const useFetch = () => {
 	const [data, setData] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 	const navigate = useNavigate();
+	const {setAuth, headers} = useContext(UserContext)
 
 	const postFetch = (end, body) => {
 		setLoading(true);
@@ -17,6 +19,7 @@ const useFetch = () => {
 				console.log(resp)
 				setData(resp.data);
 				navigate("/client");
+				setAuth(true)
 			})
 			.catch((err) => {
 				console.log(err.response.data.errors);
