c053df158c5a8941366cbacd316805f1305d47c7
hugostanley <stanleyhugo06@gmail.com>
2022-07-18 14:10:07 +0800

    Add description comment on the private route and user context usage

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import PrivateRoute from "routes/PrivateRoute";
 import { UserContext } from "utils/Context";
 
 const App = () => {
+	/* These are the necessary data needed to be saved in the local localStorage */
+	/* userData = the data response from login api call -> user ID is needed later on */
+	/* headers = this will be reused a lot of times for the other api calls */
+	/* auth = this is a state to check whether the user is logged in or not */
 	const [userData, setUserData] = useState(
 		JSON.parse(localStorage.getItem("user"))
 	);
-	const [auth, setAuth] = useState(JSON.parse(localStorage.getItem("auth")));
 	const [headers, setHeaders] = useState(
 		JSON.parse(localStorage.getItem("headers"))
 	);
+	const [auth, setAuth] = useState(JSON.parse(localStorage.getItem("auth")));
 
+	/* This useEffect happens whenever a state is being changed or manipulated */
+	/* What this does is it basically updates the local storage whenver there is a change in the states */
 	useEffect(() => {
 		localStorage.setItem("auth", JSON.stringify(auth));
 		localStorage.setItem("user", JSON.stringify(userData));
 		localStorage.setItem("headers", JSON.stringify(headers));
 	}, [auth, userData, headers]);
 
+	/*UserContext is placed at the very top of the app so it could be accessed by components that need it*/
+				/*Private route is to secure the website from being accessed even if you are logged out, if auth is true, can proceed to client page, else redirect to landing page*/
 	return (
 		<UserContext.Provider
 			value={{ userData, setUserData, auth, setAuth, headers, setHeaders }}
