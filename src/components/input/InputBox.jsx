6b46f8b611ef9c744dcb73d63eb34f7854e00be9
hugostanley <stanleyhugo06@gmail.com>
2022-07-18 15:45:36 +0800

    update conflicting properties

--- src/components/input/InputBox.jsx
+++ src/components/input/InputBox.jsx
@@ -7,24 +7,34 @@ import {
 	InputRightElement,
 } from "@chakra-ui/react";
 import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
-const InputBox = ({handleChange, id, title, type, placeholder, showPass, setShowPass }) => {
+const InputBox = ({
+	value,
+	id,
+	title,
+	type,
+	placeholder,
+	showPass,
+	setShowPass,
+	onStateChange
+}) => {
 	const handleClick = () => setShowPass(!showPass);
 	return (
 		<>
 			{type !== "password" ? (
 				<Box>
 					<FormLabel htmlFor={id}>{title}</FormLabel>
-					<Input onChange={handleChange} id={id} type={type} placeholder={placeholder} />
+					<Input onChange={onStateChange} value={value} id={id} type={type} placeholder={placeholder} />
 				</Box>
 			) : (
 				<Box>
 					<FormLabel htmlFor="password">{title}</FormLabel>
 					<InputGroup>
 						<Input
-							id="password"
+							id={id}
 							type={showPass ? "text" : "password"}
 							placeholder={placeholder}
-							onChange={handleChange}
+							value={value}
+							onChange={onStateChange}
 						/>
 						<InputRightElement>
 							{showPass ? (
