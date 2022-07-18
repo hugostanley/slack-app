1473d450bd56e32ee2358517504dab7114fac15f
hugostanley <stanleyhugo06@gmail.com>
2022-07-18 14:01:19 +0800

    Add description comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { createContext, useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/index";
 import Client from "./pages/Client";
 import PrivateRoute from "routes/PrivateRoute";
-import {UserContext} from 'utils/Context'
+import { UserContext } from "utils/Context";
 
 const App = () => {
-	const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('user')));
-	const [auth, setAuth] = useState(JSON.parse(localStorage.getItem('auth')));
-	const [headers, setHeaders] = useState(JSON.parse(localStorage.getItem('headers')));
+	const [userData, setUserData] = useState(
+		JSON.parse(localStorage.getItem("user"))
+	);
+	const [auth, setAuth] = useState(JSON.parse(localStorage.getItem("auth")));
+	const [headers, setHeaders] = useState(
+		JSON.parse(localStorage.getItem("headers"))
+	);
 
-	useEffect(()=>{
-		localStorage.setItem('auth', JSON.stringify(auth))
-		localStorage.setItem('user', JSON.stringify(userData))
-		localStorage.setItem('headers', JSON.stringify(headers))
-	},[auth, userData, headers])
-	
+	useEffect(() => {
+		localStorage.setItem("auth", JSON.stringify(auth));
+		localStorage.setItem("user", JSON.stringify(userData));
+		localStorage.setItem("headers", JSON.stringify(headers));
+	}, [auth, userData, headers]);
+
 	return (
-		<UserContext.Provider value={{userData, setUserData, auth, setAuth, headers, setHeaders}}>
+		<UserContext.Provider
+			value={{ userData, setUserData, auth, setAuth, headers, setHeaders }}
+		>
 			<Routes>
 				<Route index path="/" element={<LandingPage />} />
 				<Route element={<PrivateRoute log={auth} />}>
