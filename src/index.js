21b0123b58d9da20b4fd062ee3f9e5195a924aab
hugostanley <stanleyhugo06@gmail.com>
2022-07-21 23:03:20 +0800

    Remove react strictmode

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
 		<ChakraProvider>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</ChakraProvider>
-	</React.StrictMode>
 );
