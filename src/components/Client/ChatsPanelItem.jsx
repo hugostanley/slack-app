2417ada0054aeffbd48b1acb47e2ae9e2ed73a93
hugostanley <stanleyhugo06@gmail.com>
2022-07-23 01:54:02 +0800

    Create add user in channel modal

--- src/components/Client/ChatsPanelItem.jsx
+++ src/components/Client/ChatsPanelItem.jsx
@@ -1,15 +1,45 @@
-import React from "react";
-import { Avatar, AvatarGroup, Flex, Heading, Text } from "@chakra-ui/react";
-
+import React, { useContext, useState } from "react";
+import {
+	Alert,
+	AlertIcon,
+	Avatar,
+	AvatarGroup,
+	Button,
+	Center,
+	Flex,
+	FormLabel,
+	Heading,
+	IconButton,
+	Input,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Modal,
+	ModalBody,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+	Text,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { AddIcon, InfoIcon } from "@chakra-ui/icons";
+import { ConvoContext } from "utils/Context";
+import ProgressModal from "pages/LandingPage/components/ProgressModal";
 const ChatsPanel = ({
+	handleAdd,
 	type,
 	message,
 	name,
-	source,
 	handleSelect,
 	email,
 	id,
+	success,
+	error,
+	loading,
 }) => {
+	const [selectedUser, setSelectedUser] = useState("");
+	const { allUsers } = useContext(ConvoContext);
 	const handleClick = () => {
 		if (type === "Channel") {
 			handleSelect({ id, type, name });
@@ -17,36 +47,102 @@ const ChatsPanel = ({
 			handleSelect({ email, id, type: "User" });
 		}
 	};
+	const handlePress = () => {
+		if (selectedUser) {
+			let idx = allUsers.findIndex((item) => item.email === selectedUser);
+			console.log(allUsers[idx].id, id);
+			handleAdd(id, allUsers[idx].id);
+		}
+		setSelectedUser("");
+	};
+	const { onClose, onOpen, isOpen } = useDisclosure();
 	return (
-		<Flex
-			p={2}
-			bg={"gray.100"}
-			borderRadius={10}
-			cursor="pointer"
-			onClick={handleClick}
-		>
-			{type === "Channel" ? (
-				<AvatarGroup size={"sm"} max={2}>
-					<Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
-					<Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
-					<Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
-					<Avatar name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
-					<Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
-				</AvatarGroup>
-			) : (
-				<Avatar
-					size={"sm"}
-					name={name.charAt(0)}
-					src={"https://bit.ly/broken-link"}
-				/>
-			)}
-			<Flex flexDir="column" ml={2} justifyContent="center">
-				<Heading as={"h4"} fontSize={"xs"}>
-					{name}
-				</Heading>
-				<Text fontSize={"x-small"}> {message} </Text>
+		<>
+			<Flex
+				p={2}
+				bg={"gray.100"}
+				borderRadius={10}
+				cursor="pointer"
+				onClick={handleClick}
+				maxW="20rem"
+			>
+				{type === "Channel" ? (
+					<AvatarGroup size={"sm"} max={2}>
+						<Avatar src="https://biflorence" />
+						<Avatar src="https://biadebayo" />
+					</AvatarGroup>
+				) : (
+					<Avatar
+						size={"sm"}
+						name={name.charAt(0)}
+						src={"https://bit.ly/broken-link"}
+					/>
+				)}
+				<Flex flexGrow={"1"} flexDir="column" ml={2} justifyContent="center">
+					<Heading as={"h4"} fontSize={"xs"}>
+						{name}
+					</Heading>
+					<Text fontSize={"x-small"}> {message} </Text>
+				</Flex>
+				{type === "Channel" ? (
+					<Menu flexGrow="2">
+						<MenuButton
+							variant={"outlined"}
+							as={IconButton}
+							icon={<InfoIcon />}
+						/>
+						<MenuList>
+							<MenuItem icon={<AddIcon />} onClick={onOpen}>
+								Add user
+							</MenuItem>
+						</MenuList>
+					</Menu>
+				) : null}
 			</Flex>
-		</Flex>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					{loading ? (
+						<ProgressModal />
+					) : (
+						<>
+							<ModalHeader>Add user to {name}</ModalHeader>
+							<ModalBody mb={"3"}>
+								<FormLabel>User email</FormLabel>
+								<Input
+									value={selectedUser}
+									onChange={(e) => setSelectedUser(e.target.value)}
+									list="usersList"
+								/>
+								<datalist id="usersList">
+									{allUsers.map((item) => {
+										return <option value={item.email} />;
+									})}
+								</datalist>
+							</ModalBody>
+							<Center>
+								<Button onClick={handlePress} mb="5">
+									{" "}
+									Add user{" "}
+								</Button>
+							</Center>
+							{success && (
+								<Alert status="success" marginTop={"5"}>
+									<AlertIcon />
+									User added!
+								</Alert>
+							)}
+							{error && (
+								<Alert status="error" marginTop={"5"}>
+									<AlertIcon />
+									{error}
+								</Alert>
+							)}
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
 	);
 };
 
